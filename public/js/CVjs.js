665c3df357b9013eb232d1409c8101c1d47b9e40
siegfried Chapotin <[siegfried.chapotin@laposte.net]>
2017-12-15 10:57:28 +0100

    restrucuration total du site et inclusion d'une page compétence

--- public/js/CVjs.js
+++ public/js/CVjs.js
@@ -1,4 +1,15 @@
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
-})
+});
+
+
+$('#accordion').on('shown.bs.collapse hidden.bs.collapse', function (e) {
+         $(e.target).prev('.panel-heading').find("span.glyphicon").toggleClass('glyphicons-601-chevron-up.png glyphicons-602-chevron-down.png',200, "easeOutSine" );
+});
+
+$("button").click(function () {
+	$("ul").toggle(); $(this).children('.fa')
+	.toggleClass('fa-chevron-up fa-chevron-down'); 
+	}); 
+	
